9f34066cfb3f84ce2b59a0866a091445c2d64725
ctanseco <ctanseco@gmail.com>
2021-03-26 15:32:25 -0700

    Added 'am-i-eligible' (new york) API.

--- vax_availability_extractor.py
+++ vax_availability_extractor.py
@@ -1,7 +1,9 @@
 import requests
 import json
 
+# request list of vaccine providers in NY from the am-i-eligible API
 response = requests.get("https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers")
 
-print (json.dumps(response.json(), indent=4, sort_keys=True))
+# prints am-i-eligible json
+print(json.dumps(response.json(), indent=4, sort_keys=True))
 
