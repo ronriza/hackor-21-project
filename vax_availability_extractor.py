87359c9002a66acb5898b34b5360b796f5fccc18
fahad <awan.fahad@gmail.com>
2021-03-26 17:52:43 -0500

    Added writing to csv

--- vax_availability_extractor.py
+++ vax_availability_extractor.py
@@ -1,7 +1,12 @@
 import requests
 import json
+import pandas
+
 
 response = requests.get("https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers")
 
 print (json.dumps(response.json(), indent=4, sort_keys=True))
 
+data=json.dumps(response.json(), indent=4, sort_keys=True)
+csv_output_obj=pandas.read_json(data, orient="columns")
+csv_output_obj.to_csv('data.csv')
