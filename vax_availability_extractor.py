e310225230954dfed571b85ff046a5dea07892d8
Ron Riza <ronriza91@gmail.com>
2021-03-26 20:45:29 -0400

    edited json object

--- vax_availability_extractor.py
+++ vax_availability_extractor.py
@@ -6,9 +6,10 @@ import pandas
 # request list of vaccine providers in NY from the am-i-eligible API
 response = requests.get("https://am-i-eligible.covid19vaccine.health.ny.gov/api/list-providers")
 
-# prints am-i-eligible json
-print(json.dumps(response.json(), indent=4, sort_keys=True))
 
-data=json.dumps(response.json(), indent=4, sort_keys=True)
+# prints am-i-eligible json
+print(json.dumps(response.json()['providerList'], indent=4, sort_keys=True))
+
+data=json.dumps(response.json()['providerList'], indent=4, sort_keys=True)
 csv_output_obj=pandas.read_json(data, orient="columns")
 csv_output_obj.to_csv('data.csv')
