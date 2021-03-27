aeddd3d0b32cda079bd7f78a4a5e401abc192573
donatosaur <66536458+donatosaur@users.noreply.github.com>
2021-03-27 09:09:55 -0400

    Implemented getters & properties for Person

--- user_input.py
+++ user_input.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True)
 class Person:
     """Represents a Person with the necessary attributes to notify the person of vaccine availability """
@@ -8,10 +9,37 @@ class Person:
     _radius: int
     _email: str
 
-    def getAge(self):
+    def get_age(self):
         """Returns age of person"""
         return self._age
 
+    def get_person_zipcode(self):
+        """Returns zipcode of person"""
+        return self._person_zipcode
+
+    def get_radius(self):
+        """Returns radius of person"""
+        return self._radius
+
+    def get_email(self):
+        """Returns email of person"""
+        return self._email
+
+    @property
+    def age(self) -> int:
+        return self._age
+
+    @property
+    def zip_code(self) -> int:
+        return self._person_zipcode
+
+    @property
+    def radius(self) -> int:
+        return self._radius
+
+    @property
+    def email(self) -> str:
+        return self._email
 
 # test = Person(12, 8854, 3, 'cat@gmail.com')
 # print(test.__hash__())
