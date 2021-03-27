ba89ea4ea95397ab0f5839fbc6e6189f6bd925b4
Ron Riza <ronriza91@gmail.com>
2021-03-26 23:51:44 -0400

    added phone and email notification logic

--- sms_alert.py
+++ sms_alert.py
@@ -3,16 +3,16 @@ import os
 from twilio.rest import Client
 
 
-def phone_notify(self):
+def twilio_notify(person, locations):
     account_sid = os.environ['twilio_sid']
     auth_token = os.environ['twilio_auth_token']
     client = Client(account_sid, auth_token)
 
     message = client.messages \
         .create(
-        body="Vaccine Available!",
+        body="Vaccine Available!\n" + locations,
         from_='+13343360125',
-        to='+1' #phone number
+        to='+1' + person._number
     )
 
     print(message.sid)
